f82d433464b818204ebdbf6035ad66848ec3c8a8
aleclawlor <lawlorab@bc.edu>
2022-01-03 17:49:06 -0500

    attempting to fix deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,13 @@ const emailRoutes = require('./emailRoutes')
 
 router.use('/email', emailRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 module.exports = router 
